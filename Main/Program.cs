dd4a150d4a5181a75342b443b367755271758ed7
Akira <kuramal12@msn.com>
2022-11-10 15:56:09 +0800

    Serilog Plugin for Logger

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,8 +1,15 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddNewtonsoftJson();
+#region Use Serilog
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+//builder.Host.UseSerilog();
+#endregion
+
+builder.Services.AddControllers(option => { }).AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
