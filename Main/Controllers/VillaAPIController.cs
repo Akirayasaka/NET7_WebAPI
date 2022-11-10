e1648efd13eff0346934b26ade7e7a06667ae6f8
Akira <kuramal12@msn.com>
2022-11-10 11:07:05 +0800

    Add Status Code

--- Main/Controllers/VillaAPIController.cs
+++ Main/Controllers/VillaAPIController.cs
@@ -10,15 +10,18 @@ namespace Main.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("{id: int}")]
-        public VillaDTO GetVilla(int id)
+        [HttpGet("{id:int}")]
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+            if (id == 0) { return BadRequest(); }
+            VillaDTO villa = VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+            if (villa == null) { return NotFound(); }
+            return Ok(villa);
         }
     }
 }
