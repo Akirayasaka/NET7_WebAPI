bbf5e21c78c6ca556655df8a88c1347feefe5cf1
Akira <kuramal12@msn.com>
2022-11-10 16:57:30 +0800

    Setting DbContext & Connection String;

---
+++ Main/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Main.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Main.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        // 對應資料庫Table名稱
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
--- Main/Models/Villa.cs
+++ Main/Models/Villa.cs
@@ -1,11 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Main.Models
 {
     public class Villa
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Details { get; set; }
         public double Rate { get; set; }
