f0090f7c831d2c8f5d74fac58af9634b851e6a2e
Akira <kuramal12@msn.com>
2022-11-15 23:11:33 +0800

    Add Controller to Call Villa API

---
+++ Web/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Web.Services.IServices;
+using InjectAttribute = Microsoft.Extensions.DependencyInjection.InjectAttribute;
+
+namespace Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        [Inject] protected readonly IVillaService _villaService;
+        [Inject] protected readonly IMapper _mapper;
+
+        public BaseController(IServiceProvider provider)
+        {
+            provider.Inject(this);
+        }
+    }
+}
---
+++ Web/Controllers/VillaController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Web.Models;
+using Web.Models.Dto.Villa;
+using Web.Services.IServices;
+
+namespace Web.Controllers
+{
+    public class VillaController : BaseController
+    {
+        public VillaController(IServiceProvider provider) : base(provider)
+        {
+        }
+
+        public async Task<IActionResult> IndexVilla()
+        {
+            List<VillaDTO> list = new();
+            var response = await _villaService.GetAllAsync<ApiResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
