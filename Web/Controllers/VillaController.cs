5cb4057c0929e23920070292fe086e2d4e0f0984
Akira <kuramal12@msn.com>
2022-11-18 23:16:38 +0800

    Create Villa UI

--- Web/Controllers/VillaController.cs
+++ Web/Controllers/VillaController.cs
@@ -7,21 +7,41 @@ using Web.Services.IServices;
 
 namespace Web.Controllers
 {
-    public class VillaController : BaseController
-    {
-        public VillaController(IServiceProvider provider) : base(provider)
-        {
-        }
+	public class VillaController : BaseController
+	{
+		public VillaController(IServiceProvider provider) : base(provider)
+		{
+		}
 
-        public async Task<IActionResult> IndexVilla()
-        {
-            List<VillaDTO> list = new();
-            var response = await _villaService.GetAllAsync<ApiResponse>();
-            if (response != null && response.IsSuccess)
-            {
-                list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
-            }
-            return View(list);
-        }
-    }
+		public async Task<IActionResult> IndexVilla()
+		{
+			List<VillaDTO> list = new();
+			var response = await _villaService.GetAllAsync<ApiResponse>();
+			if (response != null && response.IsSuccess)
+			{
+				list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+			}
+			return View(list);
+		}
+
+		public async Task<IActionResult> CreateVilla()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> CreateVilla(VillaCreateDTO model)
+		{
+			if (ModelState.IsValid)
+			{
+				var response = await _villaService.CreateAsync<ApiResponse>(model);
+				if (response != null && response.IsSuccess)
+				{
+					return RedirectToAction(nameof(IndexVilla));
+				}
+			}
+			return View(model);
+		}
+	}
 }
