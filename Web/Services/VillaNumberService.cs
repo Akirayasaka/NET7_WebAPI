7db2d9bdc76f849c2ca5ff7fc3f0d6f71737f0b7
Akira <kuramal12@msn.com>
2022-11-23 10:55:59 +0800

    Add VillaNumberService

---
+++ Web/Services/VillaNumberService.cs
@@ -0,0 +1,65 @@
+﻿using Web.Models;
+using Web.Models.Dto.VillaNumber;
+using Web.Services.IServices;
+
+namespace Web.Services
+{
+    public class VillaNumberService : BaseService, IVillaNumberService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string villaUrl;
+
+        public VillaNumberService(IHttpClientFactory clientFactory, IConfiguration configuration): base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+            villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+        }
+
+        public Task<T> CreateAsync<T>(VillaNumberCreateDTO dto)
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = dto,
+                Url = villaUrl + "/api/villaNumberAPI"
+            });
+        }
+
+        public Task<T> DeleteAsync<T>(int id)
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType= StaticDetails.ApiType.DELETE,
+                Url = villaUrl + "/api/villaNumberAPI/" + id
+            });
+        }
+
+        public Task<T> GetAllAsync<T>()
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = villaUrl + "/api/villaNumberAPI"
+            });
+        }
+
+        public Task<T> GetAsync<T>(int id)
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = villaUrl + "/api/villaNumberAPI/" + id
+            });
+        }
+
+        public Task<T> UpdateAsync<T>(VillaNumberUpdateDTO dto)
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = StaticDetails.ApiType.PUT,
+                Data = dto,
+                Url = villaUrl + "/api/villaNumberAPI/" + dto.VillaNo
+            });
+        }
+    }
+}
