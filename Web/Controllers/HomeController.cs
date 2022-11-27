c3662cf8147ddd0d833359fad01611a1ecd816f5
Akira <kuramal12@msn.com>
2022-11-27 19:39:29 +0800

    Villa Home Index Page

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,32 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 using Web.Models;
+using Web.Models.Dto.Villa;
 
 namespace Web.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : BaseController
+	{
+		public HomeController(IServiceProvider provider) : base(provider) { }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public async Task<IActionResult> Index()
+		{
+			List<VillaDTO> list = new();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+			var response = await _villaService.GetAllAsync<ApiResponse>();
+			if (response != null && response.IsSuccess)
+			{
+				list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+			}
+			return View(list);
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+	}
 }
