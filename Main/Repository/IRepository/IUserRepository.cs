63145f690f643674a08d9245a9b15d6a571c3b22
Akira <kuramal12@msn.com>
2022-12-09 23:19:39 +0800

    Add User Repository

---
+++ Main/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Main.Models;
+using Main.Models.Dto.Login;
+
+namespace Main.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<User> Register(RegisterationRequestDTO registerationRequestDTO);
+    }
+}
---
+++ Main/Repository/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using Main.Data;
+using Main.Models;
+using Main.Models.Dto.Login;
+using Main.Repository.IRepository;
+
+namespace Main.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public UserRepository(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
