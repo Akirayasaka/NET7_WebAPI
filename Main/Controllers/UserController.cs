643c3c2fd32bd6af408943b3f0f9cea2cb66d6c4
Akira <kuramal12@msn.com>
2022-12-15 22:37:23 +0800

    Add API Register

--- Main/Controllers/UserController.cs
+++ Main/Controllers/UserController.cs
@@ -23,7 +23,32 @@ namespace Main.Controllers
             }
             _response.IsSuccess = true;
             _response.StatusCode = HttpStatusCode.OK;
-            _response.Result= loginResponse;
+            _response.Result = loginResponse;
+            return Ok(_response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterationRequestDTO model)
+        {
+            bool userExsist = _unitOfWork.User.IsUniqueUser(model.UserName);
+            if (!userExsist)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Messages.Add("Username already exists");
+                return BadRequest(_response);
+            }
+
+            var user = _unitOfWork.User.Register(model);
+            if (user == null)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Messages.Add("Register Error");
+                return BadRequest(_response);
+            }
+            _response.IsSuccess = true;
+            _response.StatusCode = HttpStatusCode.OK;
             return Ok(_response);
         }
     }
