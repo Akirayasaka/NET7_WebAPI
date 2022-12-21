8f0693b59223ddd7412675dd5c429aaed9b663c7
Akira <kuramal12@msn.com>
2022-12-21 23:53:36 +0800

    Add Auth Service

---
+++ Web/Services/AuthService.cs
@@ -0,0 +1,28 @@
+﻿using Web.Models;
+using Web.Models.Dto.Login;
+using Web.Services.IServices;
+
+namespace Web.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string villaUrl;
+
+        public AuthService(IHttpClientFactory httpClient, IConfiguration configuration) : base(httpClient)
+        {
+            _clientFactory = httpClient;
+            villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+        }
+
+        public Task<T> LoginAsync<T>(LoginRequestDTO requestDTO)
+        {
+            return SendAsync<T>(new ApiRequest() { ApiType = StaticDetails.ApiType.POST, Data = requestDTO, Url = villaUrl + "/api/User/Login"});
+        }
+
+        public Task<T> RegisterAsync<T>(RegisterationRequestDTO requestDTO)
+        {
+            return SendAsync<T>(new ApiRequest() { ApiType = StaticDetails.ApiType.POST, Data = requestDTO, Url = villaUrl + "/api/User/Register" });
+        }
+    }
+}
---
+++ Web/Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Web.Models.Dto.Login;
+
+namespace Web.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<T> LoginAsync<T>(LoginRequestDTO requestDTO);
+        Task<T> RegisterAsync<T>(RegisterationRequestDTO requestDTO);
+    }
+}
