ddf40fd9db89fad1a4d69d446965283eba0fdbe8
Akira <kuramal12@msn.com>
2022-12-23 23:25:17 +0800

    Add Register Action

--- Web/Controllers/AuthController.cs
+++ Web/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
 using Web.Models.Dto.Login;
 
 namespace Web.Controllers
@@ -20,6 +21,24 @@ namespace Web.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Login(LoginRequestDTO loginRequestDTO)
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterationRequestDTO requestDTO)
+        {
+            ApiResponse result = await _authService.RegisterAsync<ApiResponse>(requestDTO);
+            if (result != null & result.IsSuccess)
+            {
+                return RedirectToAction("Login");
+            }
             return View();
         }
     }
